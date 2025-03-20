d996458a678873ad19471fe7e7005f2c57110ae1
Mankirat Singh <w0857232@myscc.ca>
2025-03-19 21:30:59 -0400

    Started Assignment 8

--- app/src/main/java/com/zybooks/assignment7/MainActivity.kt
+++ app/src/main/java/com/zybooks/assignment7/MainActivity.kt
@@ -2,6 +2,7 @@ package com.zybooks.assignment7
 
 import android.app.DatePickerDialog
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -18,12 +19,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var submitButton: Button
     private lateinit var deleteButton: Button
 
+
+
     private lateinit var expenseAdapter: ExpenseAdapter
     private val expenseArray = mutableListOf<Expense>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("ActivityLifecycle", "onCreate called")
+
 
         expenseName = findViewById(R.id.expenseName)
         expenseAmount = findViewById(R.id.expenseAmount)
@@ -88,6 +93,31 @@ class MainActivity : AppCompatActivity() {
         val myFormat = "yyyy-MM-dd"
         val sdf = SimpleDateFormat(myFormat, Locale.CANADA)
         expenseDate.setText(sdf.format(calendar.time))
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("ActivityLifecycle", "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("ActivityLifecycle", "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("ActivityLifecycle", "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("ActivityLifecycle", "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("ActivityLifecycle", "onDestroy called")
     }
 
 }
